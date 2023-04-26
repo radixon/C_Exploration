9282d1638a7b2786bbe92cef626a4f55f6b10dbc
Dixon <59415488+radixon@users.noreply.github.com>
2023-04-26 14:09:46 -0400

    Update dweight.c

--- Chapter2/dweight.c
+++ Chapter2/dweight.c
@@ -3,22 +3,29 @@
  *  Lesson:     Chapter 2 of K.N. King C Programming: A Modern Approach
 */
 
+    // Directives
     #include <stdio.h>
     #define INCHES_PER_POUND 166
 
+    // Function
     int main(void){
+        // Declarations
         int height, length, width, volume, weight;
 
+        // Assignment
         height = 8;
         length = 12;
         width = 10;
         
+        // Statements
         volume = height * length * width;
         weight = (volume + INCHES_PER_POUND)/INCHES_PER_POUND;
-
+        
+        // Printing the Value of Variables
         printf("Dimensions: %dx%dx%d\n", length, width, height);
         printf("Volume (cubic inches): %d\n", volume);
         printf("Dimensional weight (pounds): %d\n", weight);
-
+        
+        // Return Statement
         return 0;
     }
